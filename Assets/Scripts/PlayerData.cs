c53f095041542b8789584edc75b1dcff6e2f0e1d
DylanSchmidt-Cunningham <da_cunningham@laurentian.ca>
2018-04-05 18:28:25 -0400

    added assets, objects, and player data

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="PlayerData")]
+public class PlayerData : ScriptableObject {
+
+    [HideInInspector]
+    public int score = 0;
+
+    public KeyCode up;
+    public KeyCode down;
+    public Transform paddle;
+
+}
