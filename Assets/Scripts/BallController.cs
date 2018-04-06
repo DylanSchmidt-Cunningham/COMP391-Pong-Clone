1ef3028bf99692e23a786486418d0d5462f8322a
DylanSchmidt-Cunningham <da_cunningham@laurentian.ca>
2018-04-06 03:16:54 -0400

    added player controls

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public float x_force = 80f;
+    public float y_force = 10f;
+
+    private Rigidbody2D rBody;
+
+	// Use this for initialization
+	void Start () {
+        rBody = this.GetComponent<Rigidbody2D>();
+
+        var randomNumber = Random.Range(0, 1+1); // max exclusive
+        if(randomNumber <= 0.5f)
+        {
+            //Debug.Log("Shoot right");
+            rBody.AddForce(new Vector2(x_force, y_force));
+        }
+        else
+        {
+            //Debug.Log("Shoot left");
+            rBody.AddForce(new Vector2(-x_force, -y_force));
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Player")
+        {
+            //Debug.Log("It's working");
+            var velY = rBody.velocity.y/2 
+                + collision.collider.GetComponent<Rigidbody2D>().velocity.y/3;
+            rBody.velocity = new Vector2(rBody.velocity.x, velY);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [HideInInspector]
+    public int score = 0;
+
+    public KeyCode up;
+    public KeyCode down;
+    public float speed = 10.0f;
+
+    private Rigidbody2D rBody;
+
+    public void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(up))
+        {
+            rBody.velocity = new Vector2(0, speed);
+        }
+        else if (Input.GetKey(down))
+        {
+            rBody.velocity = new Vector2(0, -speed);
+        }
+        else
+        {
+            rBody.velocity = new Vector2(0, 0);
+        }
+	}
+}
